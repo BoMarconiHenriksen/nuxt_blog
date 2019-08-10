79badf6c164d743301cf9863f1b3fe7a9a7926c1
BoMarconiHenriksen <bomarconi@gmail.com>
2019-08-10 13:48:33 +0200

    Added async data for a single post. Used context which gives access to information. Use it insted of this.route

--- blog/pages/posts/_id/index.vue
+++ blog/pages/posts/_id/index.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="single-post-page">
         <section class="post">
-            <h1 class="post-title">Title of the Post</h1>
+            <h1 class="post-title">{{ loadedPost.title }}</h1>
             <div class="post-details">
-                <div class="post-detail">Last updated on xxxx</div>
-                <div class="post-detail">Written by NAME</div>
+                <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
+                <div class="post-detail">Written by {{ loadedPost.author }}</div>
             </div>
-            <p class="post-content">Content of the post</p>
+            <p class="post-content">{{ loadedPost.content }}</p>
         </section>
         <section class="post-feedback">
             <p>Let me know whatyou think of the post, send a mail to <a href="mailto:feedback@domain.com">feedback@domain.com</a></p>
         </section>
     </div>
 </template>
+
+<script>
+export default {
+  // Return the page after it has fetched the data from a server.
+    asyncData(context, callback) {
+        setTimeout(() => {
+        callback(null, {
+            loadedPost: 
+            {
+            id: '1', 
+            title: 'Our First Post (ID: ' + context.params.id + ')' , 
+            previewText: 'This is our first post!', 
+            author: 'Bob Samson',
+            updatedDate: new Date(),
+            content: 'Dummy text which is not the preview text!!!',
+            thumbnailLink: 'https://images.unsplash.com/photo-1556740714-a8395b3bf30f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+            }
+        });
+        }, 1500);
+    },      
+}
+</script>
+
 
 <style scoped>
 .single-post-page {
