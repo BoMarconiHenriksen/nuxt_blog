31be72081bee8a3b9ff33509bb10d5837b4157ea
BoMarconiHenriksen <bomarconi@gmail.com>
2019-08-10 13:16:35 +0200

    Added blog posts to the blog page

--- blog/pages/posts/index.vue
+++ blog/pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="posts-page">
-        <PostList />
+        <PostList :posts="loadedPosts"/>
     </div>
 </template>
 
@@ -10,7 +10,28 @@ import PostList from '@/components/Posts/PostList'
 export default {
     components: {
         PostList
-    }    
+    },
+    // Return the page after it has fetched the data from a server.
+    asyncData(context, callback) {
+        setTimeout(() => {
+        callback(null, {
+            loadedPosts: [
+            {
+            id: '1', 
+            title: 'Our First Post', 
+            previewText: 'This is our first post!', 
+            thumbnailLink: 'https://images.unsplash.com/photo-1556740714-a8395b3bf30f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+            },
+            {
+            id: '2', 
+            title: 'Our Second Post', 
+            previewText: 'This is our second post!', 
+            thumbnailLink: 'https://images.unsplash.com/photo-1556740714-a8395b3bf30f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+            }
+            ]
+        });
+        }, 1500);
+    },    
 }
 </script>
 
